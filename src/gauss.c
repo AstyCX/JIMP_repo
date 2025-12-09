2f617f057e81c783d74d1fed9416157e2f399287
Asty <medved.danila@proton.me>
2025-12-09 14:00:36 +0100

    Comments edited & small declaration fixes

--- src/gauss.c
+++ src/gauss.c
@@ -13,18 +13,21 @@ int eliminate(Matrix *mat, Matrix *b){
         return 1;
     }
 
-	for (int k = 0; k < n-1; ++k) {
+	for (k = 0; k < n-1; ++k) {
 		if (mat->data[k][k] == 0.0) {
 			return 1; // Macierz osobliwa
 		}
 
-		for (int i = k + 1; i < n; ++i) {
+		for (i = k + 1; i < n; ++i) {
+			// Obliczamy wspolczynnik eliminacji
 			double factor = mat->data[i][k] / mat->data[k][k];
+
+			// Aktualiz. wiersze
 			for (j = k; j < n; ++j) {
 				mat->data[i][j] -= factor * mat->data[k][j];
 			}
 
-			// Aktualizujemy wektor b
+			// Aktualiz. wektor b
 			b->data[i][0] -= factor * b->data[k][0];
 		}
 	}
