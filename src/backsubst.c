10297c0746c90900f61f7044124c8e07145a0574
jilejile555-tech <01205861@pw.edu.pl>
2025-12-10 20:25:36 +0100

    main code

--- src/backsubst.c
+++ src/backsubst.c
@@ -22,10 +22,19 @@ int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
     	if (mat->r != mat->c || mat->r != b->r || mat->r != x->r || b->c != 1 || x->c != 1) {
     	    return 2;
     	}
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
+				for (i = n - 1; i >= 0; i--) {
+        double s = 0.0; 
+
+        for (j = i + 1; j < n; j++) {
+            s += mat->data[i][j] * x->data[j][0];
+        }
+
+        if (mat->data[i][i] == 0.0) {
+            return 1; 
+        }
+
+        x->data[i][0] = (b->data[i][0] - s) / mat->data[i][i];
+    }
 
 				return 0;
 }
